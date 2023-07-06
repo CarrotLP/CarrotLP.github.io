5864278dd5ae46e19f696844d85c249c850ba1e1
Au-Yeung <sqb32942@fed.cclrc.ac.uk>
2023-07-06 13:24:22 +0100

    I added a toggle

--- diamond-map/diamond-map-javascript-2.js
+++ diamond-map/diamond-map-javascript-2.js
@@ -1,18 +1,28 @@
 var map = L.map('map').setView([51.57435435296581, -1.310918868757071], 17);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osm =  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="https://www.diamond.ac.uk/Home.html">Diamond Light Source</a>' + '<sup>&reg</sup>'
-}).addTo(map);
+})
+    .addTo(map);
+
+var gpsIcon = L.icon({
+    iconUrl: 'img/icon.png',
+    iconSize: [50, 50],
+    iconAnchor: [15, 33]
+});
 
 fetch ("beamlines_data.json").then((res)=>{return res.json()}).then(function(data) {
+    var overlays = {}
     for (let i = 0; i < 8; i++) {
         var dict = data[i];
         var beam = dict["beamlines"];
         var beamno = beam.length;
+        var group_markers = [];
         for (let j = 0; j < beamno; j++){
             var sepbeams = beam[j];
             coordinate = sepbeams["position"];
-            var marker = L.marker(coordinate).addTo(map);
+            var marker = L.marker(coordinate)
+            group_markers.push(marker);
             marker.bindPopup("<h3>"+sepbeams["name"]+"</h3>"+
             "<p>"+sepbeams["description"]+"</p>"+
             "<a href='"+sepbeams["url"]+"'>Click here for link</a>"
@@ -20,19 +30,20 @@ fetch ("beamlines_data.json").then((res)=>{return res.json()}).then(function(dat
 
             marker.on('mouseover', function(e) {
                 this.openPopup();
-               /* const markerHtmlStyles = `
-                width: 3rem;
-                height: 3rem;
-                left: -1.5rem;
-                top: -1.5rem;`
-                */
-            
-
             });
         }
+
+        var names = L.layerGroup(group_markers)
+        names.addTo(map);
+        grpname = dict["name"];
+        overlays[grpname] = names;
     }
+
+    // console.log(overlays)
+    var layerControl = L.control.layers(null, overlays).addTo(map);
+        
+});
     
-})
 
 const topRightCoords = [51.57168183170403, -1.3173294067382815];
 const bottomRightCoords = [51.57701619673675, -1.304454803466797];
@@ -53,7 +64,7 @@ function onLocationFound(e) {
         map.removeLayer(current_accuracy);
     }
     var radius = e.accuracy;
-    current_position = L.marker(e.latlng).addTo(map);
+    current_position = L.marker(e.latlng, {icon: gpsIcon}).addTo(map);
     current_accuracy = L.circle(e.latlng, radius).addTo(map);
 }
 
